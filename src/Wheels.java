0381830f6026984ac0130306954f9899fda9914e
bodiua <bodiua.ru94@gmail.com>
2023-02-26 14:39:35 +0200

    first commit

--- src/Wheels.java
+++ src/Wheels.java
@@ -1,10 +1,10 @@
-import java.util.Arrays;
 public class Wheels {
 
-    public static void main(String[] args)
-    {
-        int[] wheels = {1, 2, 3, 4};
-        System.out.println(wheels);
-        System.out.println(Arrays.toString(wheels));
+    public String wheelsType;
+    public Wheels (wheelsType){
+        this.wheelsType = wheelsType;
+    }
+    public String getWheelsType{
+        return wheelsType;
     }
 }
